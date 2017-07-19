7807e72a2600b1791c5f6bfe46bab8c2f7972797
dana.liu <Dana.liu@weichaishi.com>
2017-07-19 15:32:08 +0800

    [A]add some util

--- src/test/java/com/lly/test/LocalDateTest.java
+++ src/test/java/com/lly/test/LocalDateTest.java
@@ -1,11 +1,13 @@
 package com.lly.test;
 
+import com.lly.util.TimeUtil;
 import org.junit.Test;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
+import java.sql.Time;
+import java.time.*;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
 
 import static java.lang.System.out;
 
@@ -13,19 +15,52 @@ public class LocalDateTest {
 
     @Test
     public void test1(){
+//        LocalDate now = LocalDate.now();
+//        out.println(now);
+//        LocalTime time = LocalTime.now();
+//        out.println(time);
+//
+//        LocalDateTime dateTime = LocalDateTime.now();
+//        out.println(dateTime);
+//
+//        LocalDate parse = LocalDate.parse("2017-01-01");
+//        out.println(parse);
+//
+//        out.println(now.getMonthValue());
+//
+//        out.println(""+now.getYear()+now.getMonthValue() + now.getDayOfMonth());
+
+//        out.println(LocalDate.of(11,1,-1));
+
+
         LocalDate now = LocalDate.now();
+        LocalDate tomorrow = now.plusDays(1);
+        /*
+
         out.println(now);
-        LocalTime time = LocalTime.now();
-        out.println(time);
+        LocalDate plus = now.plus(1, ChronoUnit.YEARS);
+
+        out.println(localDate);
+        out.println(plus);*/
+
+        /*out.println(now.isAfter(tomorrow));
+        out.println(now.isLeapYear());//是否是闰年
+
+        out.println(Instant.now().atZone(ZoneOffset.ofHours(8)));//获取本地时间戳
+
+        out.println(Clock.system(ZoneId.of(ZoneId.SHORT_IDS.get("CTT"))).instant());
+
+        out.println(LocalDate.parse("20170706", DateTimeFormatter.BASIC_ISO_DATE));
+//        out.println(LocalDateTime.parse("2017/07/06 11:11:11",DateTimeFormatter.ofPattern("YYYY/MM/dd HH:mm:ss")));
 
         LocalDateTime dateTime = LocalDateTime.now();
-        out.println(dateTime);
+        out.println(dateTime.format(DateTimeFormatter.ofPattern("YYYY/MM/dd HH:mm:ss")));
 
-        LocalDate parse = LocalDate.parse("2017-01-01");
-        out.println(parse);
+        out.println(dateTime.format(DateTimeFormatter.ISO_DATE));*/
 
-        out.println(now.getMonthValue());
+//        out.println(TimeUtil.getNow());
+//        out.println(TimeUtil.format(LocalDateTime.now(), "YYYYMMddHHmmss"));
 
-        out.println(""+now.getYear()+now.getMonthValue() + now.getDayOfMonth());
+
     }
 }
